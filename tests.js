83429b252ce51eced54c5808e81f8eacee79102b
Jason Crites <jcrites3@hotmail.com>
2022-11-30 15:30:10 -0600

    added sayHello function to code.js and sayHello spec to tests.js

--- tests.js
+++ tests.js
@@ -13,3 +13,20 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+describe('sayHello', function()  {
+    it('should be a defined function', function() {
+      expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+       expect(typeof sayHello).toBe('string');
+    });
+    it('should return the string "Hello, Jane!" when executed', function() {
+      expect(sayHello()).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello,!" when executed', function() {
+      expect(sayHello()).toBe("Hello, !");
+    });
+    it('should return the string "Hello,!" when executed', function() {
+      expect(sayHello()).toBe("Hello, !");
+    });
+});
